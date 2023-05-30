a6240a6741e9a370d7a377bb1a17fb2518633708
“muddyrain” <qiu3291002845@gmail.com>
2023-05-30 17:46:38 +0800

    feat: 备份

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,11 @@
 import 'reflect-metadata';
 import Express from 'express';
+import MovieRouter from './routes/MovieRoute';
 
 const app = Express();
 
-app.use('/api/movie', (req, res) => {
-  res.send('hello');
-});
+app.use('/api/movie', MovieRouter);
 
 app.listen(3000, () => {
-  console.log('listening on port 3000');
+  console.log('Listening on port 3000');
 });
